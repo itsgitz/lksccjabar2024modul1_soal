a88f48133675afa68a199b8d65a380f8522dc265
itsgitz <anggit.ginanjar.dev@gmail.com>
2024-05-12 21:52:01 +0700

    feat: add script for download index.php from the gist

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# author: @itsgitz
+
+DOCUMENT_ROOT="/var/www/html"
+GIST_URL="https://gist.githubusercontent.com/itsgitz/d062c142d94eba2381eca6e1a6d9c0ed/raw/472c5ec9373b9f40930ed3c9afb093df8c23a9e7/index.php"
+
 # Add Ondrej's PPA for installing the latest PHP version
 add-apt-repository ppa:ondrej/php -y
 
@@ -14,3 +19,9 @@ apt-get install php8.3 php8.3-cli php8.3-fpm -y
 
 nginx -v
 php -v
+
+# remove default Nginx html file
+rm $DOCUMENT_ROOT/index.html
+
+# download index.php from the gist
+wget $GIST_URL -O /var/www/html/index.php
