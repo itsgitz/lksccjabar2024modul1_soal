4872d189bb3a2cbecd39b96e72956dc23587a66d
itsgitz <anggit.ginanjar.dev@gmail.com>
2024-05-13 13:40:06 +0700

    fix: nginx configuration

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# author: @itsgitz
+#
+# author: @itsgitz - <Anggit M Ginanjar>
 
 DOMAIN_NAME="modul1.kotakserver.my.id"
 DOCUMENT_ROOT="/var/www/html"
 NGINX_CONFIGURATION_DIR="/etc/nginx"
-GIST_URL="https://gist.githubusercontent.com/itsgitz/d062c142d94eba2381eca6e1a6d9c0ed/raw/472c5ec9373b9f40930ed3c9afb093df8c23a9e7/index.php"
+GIST_URL="https://gist.githubusercontent.com/itsgitz/d062c142d94eba2381eca6e1a6d9c0ed/raw"
 
 # Add Ondrej's PPA for installing the latest PHP version
 add-apt-repository ppa:ondrej/php -y
@@ -36,22 +37,47 @@ wget $GIST_URL -O $DOCUMENT_ROOT/index.php
 chown ubuntu:www-data $DOCUMENT_ROOT/index.php
 chmod 775 $DOCUMENT_ROOT/index.php
 
-cat <<EOF > $NGINX_CONFIGURATION_DIR/sites-available/default
+cat <<EOF > $NGINX_CONFIGURATION_DIR/sites-available/$DOMAIN_NAME
 server {
-    listen 80;
-    server_name $DOMAIN_NAME;
-    root /var/www/html;
+        listen 80 default_server;
+        listen [::]:80 default_server; 
 
-	access_log /var/log/nginx/$DOMAIN_NAME-access.log;
-	error_log /var/log/nginx/$DOMAIN_NAME-error.log;
+        root /var/www/html;
 
-    index index.php;
+        # Add index.php to the list if you are using PHP
+        index index.php;
 
-    location / {
-        try_files $uri $uri/ =404;
-    }
+		access_log /var/log/nginx/$DOMAIN_NAME-access.log;
+		error_log /var/log/nginx/$DOMAIN_NAME-error.log;
+
+        server_name $DOMAIN_NAME;
+
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
+
+        # pass PHP scripts to FastCGI server
+        #
+        location ~ \.php$ {
+               include snippets/fastcgi-php.conf;
+        
+               # With php-fpm (or other unix sockets):
+               fastcgi_pass unix:/run/php/php-fpm.sock; 
+        }
+
+        # deny access to .htaccess files, if Apache's document root
+        # concurs with nginx's one
+        #
+        location ~ /\.ht {
+               deny all;
+        }
 }
 EOF
+
+ln -s "$NGINX_CONFIGURATION_DIR/sites-available/$DOMAIN_NAME" \
+	"$NGINX_CONFIGURATION_DIR/sites-enabled/"
 
 # restart web server
 systemctl restart nginx
