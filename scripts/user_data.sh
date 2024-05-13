70a30ef17c30bcf274863af0a227027ec84b402f
itsgitz <anggit.ginanjar.dev@gmail.com>
2024-05-13 07:47:21 +0700

    fix: user data script - wip

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -2,7 +2,9 @@
 
 # author: @itsgitz
 
+DOMAIN_NAME="modul1.kotakserver.my.id"
 DOCUMENT_ROOT="/var/www/html"
+NGINX_CONFIGURATION_DIR="/etc/nginx"
 GIST_URL="https://gist.githubusercontent.com/itsgitz/d062c142d94eba2381eca6e1a6d9c0ed/raw/472c5ec9373b9f40930ed3c9afb093df8c23a9e7/index.php"
 
 # Add Ondrej's PPA for installing the latest PHP version
@@ -22,6 +24,34 @@ php -v
 
 # remove default Nginx html file
 rm $DOCUMENT_ROOT/index.html
+rm $DOCUMENT_ROOT/index.nginx-debian.html
+
+# remove default Nginx configuration files
+rm "$NGINX_CONFIGURATION_DIR/sites-available/default"
+rm "$NGINX_CONFIGURATION_DIR/sites-enabled/default"
 
 # download index.php from the gist
-wget $GIST_URL -O /var/www/html/index.php
+wget $GIST_URL -O $DOCUMENT_ROOT/index.php
+
+chown ubuntu:www-data $DOCUMENT_ROOT/index.php
+chmod 775 $DOCUMENT_ROOT/index.php
+
+cat <<EOF > $NGINX_CONFIGURATION_DIR/sites-available/default
+server {
+    listen 80;
+    server_name $DOMAIN_NAME;
+    root /var/www/html;
+
+	access_log /var/log/nginx/$DOMAIN_NAME-access.log;
+	error_log /var/log/nginx/$DOMAIN_NAME-error.log;
+
+    index index.php;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+}
+EOF
+
+# restart web server
+systemctl restart nginx
